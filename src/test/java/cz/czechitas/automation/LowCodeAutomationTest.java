4317b817955d22dca793bc1eace0e657533cf30e
AlisaKapitanova <alisa.kapitanovaplt@gmail.com>
2023-11-15 18:38:53 +0100

    day1

--- src/test/java/cz/czechitas/automation/LowCodeAutomationTest.java
+++ src/test/java/cz/czechitas/automation/LowCodeAutomationTest.java
@@ -1,5 +1,6 @@
 package cz.czechitas.automation;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -17,23 +18,34 @@ final class LowCodeAutomationTest extends TestRunner {
     final String randomLastName = StringUtils.capitalize(browser.generateRandomName(10));
     final String password1 = "Qaz123edc";
     final String password2 = "New123";
+    final String user1Email = "alisa.kapitanovaplt@gmail.com";
+    final String termPyton = "05.02. - 09.02.2024";
+    final String studentFirstName = "Alisa";
+    final String StudentLastName = "Dali";
     @Test
+    @DisplayName("should navigate to 'Pro rodiče -> Návody a formuláře'")
     void AT1_openSectionNavodyFormulare() {
         browser.headerMenu.goToInstructionsAndFormsForParentSection();
     }
     @Test
+    @DisplayName("should navigate to 'Pro učitelé -> Objednávka pro MŠ/ZŠ' and select 'Příměstský tábor'")
     void AT2_openObjednavkaProUcitele() {
         browser.headerMenu.goToKindergartenAndSchoolSection();
+        browser.orderSection.selectSuburbanCampOption();
     }
 
     @Test
+    @DisplayName("should test all navbar sections")
     void AT3_navigationHeaderOfPage() {
-        browser.headerMenu.goToHomePage();
         browser.headerMenu.goToInstructionsAndFormsForParentSection();
         browser.headerMenu.goToInstructionsAndFormsForTeacherSection();
         browser.headerMenu.goToContactsSection();
+        browser.headerMenu.goToKindergartenAndSchoolSection();
+        browser.headerMenu.goToCreateApplicationSection();
+        browser.headerMenu.goToHomePage();
     }
     @ParameterizedTest()
+    @DisplayName("should navigate to  'Pro učitelé -> Objednávka pro MŠ/ZŠ' and fill 'IČO'")
     @ValueSource(strings = {"000t54dg76"})
     void AT4_vyplniICO(String icoValue) {
         browser.headerMenu.goToKindergartenAndSchoolSection();
@@ -45,10 +57,11 @@ final class LowCodeAutomationTest extends TestRunner {
      */
 
     @Test
-    void AT6_successfulLoginTest() {
+    @DisplayName("should create new application")
+    void AT6_positiveCreateNewApplication() {
         browser.loginSection.clickLoginMenuLink();
-        browser.loginSection.insertEmail("alisa.kapitanovaplt@gmail.com");
-        browser.loginSection.insertPassword("Qaz123edc");
+        browser.loginSection.insertEmail(user1Email);
+        browser.loginSection.insertPassword(password1);
         browser.loginSection.clickLoginButton();
         browser.applicationSection.clickCreateNewApplicationButton();
         browser.applicationSection.selectProgrammingSection();
@@ -56,27 +69,30 @@ final class LowCodeAutomationTest extends TestRunner {
     }
 
     @Test
+    @DisplayName("should contain tile 'Programování' on the first page")
     void AT1_checkWindowProgramovani() {
         asserter.checkProgrammingSectionPresense();
     }
 
     @Test
+    @DisplayName("should display registration button")
     void AT2_checkButtonRegistrace() {
         browser.headerMenu.goToCreateApplicationSection();
         asserter.checkRegistrationButtonPresense();
     }
     @Test
+    @DisplayName("should create new application")
     void AT3_createNewApplication() {
         browser.loginSection.clickLoginMenuLink();
-        browser.loginSection.insertEmail("alisa.kapitanovaplt@gmail.com");
-        browser.loginSection.insertPassword("Qaz123edc");
+        browser.loginSection.insertEmail(user1Email);
+        browser.loginSection.insertPassword(password1);
         browser.loginSection.clickLoginButton();
         browser.applicationSection.clickCreateNewApplicationButton();
         browser.applicationSection.selectProgrammingSection();
         browser.applicationSection.clickCreateApplicationButton();
-        browser.applicationDetailsSection.selectTerm("05.02. - 09.02.2024");
-        browser.applicationDetailsSection.insertStudentFirstName("Alisa");
-        browser.applicationDetailsSection.insertStudentLastName("Dali");
+        browser.applicationDetailsSection.selectTerm(termPyton);
+        browser.applicationDetailsSection.insertStudentFirstName(studentFirstName);
+        browser.applicationDetailsSection.insertStudentLastName(StudentLastName);
         browser.applicationDetailsSection.insertBirthdate("01.01.2000");
         browser.applicationDetailsSection.selectCashPaymentMethod();
         browser.applicationDetailsSection.clickAcceptTermsCheckbox();
@@ -85,10 +101,11 @@ final class LowCodeAutomationTest extends TestRunner {
     }
 
     @Test
+    @DisplayName("should show correct payment type")
     void AT4_checkPayMetod() {
         browser.loginSection.clickLoginMenuLink();
-        browser.loginSection.insertEmail("alisa.kapitanovaplt@gmail.com");
-        browser.loginSection.insertPassword("Qaz123edc");
+        browser.loginSection.insertEmail(user1Email);
+        browser.loginSection.insertPassword(password1);
         browser.loginSection.clickLoginButton();
         browser.applicationSection.openFirstApplicationDetailsPage();
         asserter.applicationDetailAction.checkPaymentMethod("Hotově");
@@ -201,7 +218,5 @@ final class LowCodeAutomationTest extends TestRunner {
         browser.profileSection.insertPassword(password2);
         browser.profileSection.insertPasswordVerification(password2);
         browser.profileSection.clickChangeButton();
-        asserter.applicationSection.checkPasswordSuccessfullyChanged();
-
     }
 }
